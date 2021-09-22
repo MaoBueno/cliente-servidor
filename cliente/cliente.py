fb6fdac4fb04e94284386aa8c7e578bccad1a7bd
MaoBueno <mauroalternativo519@gmail.com>
2021-09-22 18:08:41 -0500

    Funcion download con argumentos

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -22,8 +22,16 @@ if operacion == "upload":
     with open (archivo, 'rb') as f:
         byte = f.read()
         s.send_multipart([byte])
-
     m= s.recv_string()
-
-print(m)
-
+elif operacion == 'download':
+    s.send_string(user)
+    s.recv_string()
+    s.send_string(operacion)
+    s.recv_string()
+    s.send_string(archivo)
+    s.recv_string()
+    with open(archivo, 'wb') as f:
+        s.send_string('')
+        byte = s.recv_multipart()
+        f.write(byte[0])
+    s.send_string('Recibido')
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -16,5 +16,10 @@ if operacion == "upload":
     with open(archivo, 'wb') as f:
         byte = s.recv_multipart()
         f.write(byte[0])
-
     s.send_string('Recibido')
+elif operacion == 'download':
+    s.recv_string()
+    with open (archivo, 'rb') as f:
+        byte = f.read()
+        s.send_multipart([byte])
+    m= s.recv_string()
