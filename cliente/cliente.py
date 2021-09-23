a4927233b7227b23e0d01ecd2f7eb071dacc3fb9
MaoBueno <mauroalternativo519@gmail.com>
2021-09-22 20:01:03 -0500

    Funcion list

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,6 +1,7 @@
 import zmq      # Provee la comunocación a través de sockets
 import sys
 
+
 context = zmq.Context()
 
 # Crea un socket y lo conecta a tarvés del protocolo tcp con el equipo local en el puerto 8001
@@ -9,29 +10,30 @@ s.connect('tcp://localhost:8001')
 
 user = sys.argv[1]
 operacion = sys.argv[2]
-archivo = sys.argv[3]
+
+s.send_string(user)
+s.recv_string()
+s.send_string(operacion)
+s.recv_string()
 
 
 if operacion == "upload":
-    s.send_string(user)
-    s.recv_string()
-    s.send_string(operacion)
-    s.recv_string()
+    archivo = sys.argv[3]
     s.send_string(archivo)
     s.recv_string()
     with open (archivo, 'rb') as f:
         byte = f.read()
         s.send_multipart([byte])
-    m= s.recv_string()
 elif operacion == 'download':
-    s.send_string(user)
-    s.recv_string()
-    s.send_string(operacion)
-    s.recv_string()
+    archivo = sys.argv[3]
     s.send_string(archivo)
     s.recv_string()
     with open(archivo, 'wb') as f:
         s.send_string('')
         byte = s.recv_multipart()
         f.write(byte[0])
-    s.send_string('Recibido')
+elif operacion == 'list':
+    s.send_string('')
+    aux = s.recv_string()
+    print (aux)
+    
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,4 +1,5 @@
 import zmq
+import os
 
 context = zmq.Context()
 
@@ -9,17 +10,21 @@ user = s.recv_string()
 s.send_string('')
 operacion = s.recv_string()
 s.send_string('')
-archivo= s.recv_string()
-s.send_string('')
 
 if operacion == "upload":
+    archivo= s.recv_string()
+    s.send_string('')
     with open(archivo, 'wb') as f:
         byte = s.recv_multipart()
         f.write(byte[0])
-    s.send_string('Recibido')
 elif operacion == 'download':
+    archivo= s.recv_string()
+    s.send_string('')
     s.recv_string()
     with open (archivo, 'rb') as f:
         byte = f.read()
         s.send_multipart([byte])
-    m= s.recv_string()
+elif operacion =='list':
+    s.recv_string()
+    s.send_string("\n".join(os.listdir('.')))
+    
